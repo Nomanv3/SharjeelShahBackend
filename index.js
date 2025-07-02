b1d86629ba8156fa8c39f6d957ccc286fecd74b7
Nomonv3 <shahnoman034@gmail.com>
2025-07-02 23:10:27 +0530

    pushing cors as well

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors') // <== import cors
 const app = express()
 const reviewRoute = require('./routes/reviewRoute')
 
-// console.log("Email:", process.env.MAIL_USER)
-// console.log("Password:", process.env.MAIL_PASSWORD) 
+// Enable CORS for all origins
+app.use(cors()) // <== enable CORS globally
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.use('/api' , reviewRoute)
+app.use('/api', reviewRoute)
 
-app.get('/' , (req,res)=>{
-    res.send("hey")
-    // console.log("the route is working fine")
+app.get('/', (req, res) => {
+  res.send("hey")
 })
 
-
-app.listen(process.env.PORT || 3000 , (err)=>{
-    if(err) return console.log(err.message)
-        return console.log(`${process.env.PORT}`)
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) return console.log(err.message)
+  return console.log(`Server running on port ${process.env.PORT || 3000}`)
 })
